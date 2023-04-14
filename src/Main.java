0350d6622fa4570e62557fe587be6b51cd1b4081
ElenaKr <kravets_2024@internet.ru>
2023-04-14 12:18:30 +0500

    Correction commit

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
         int cash = 100;
         int refill = 1300;
 
-
         int bonus;
-        if (refill>=1000) {
-            bonus = refill/100;
-        } else {bonus =0;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
         }
 
-        int totalCash = cash+refill+bonus;
+        int totalCash = cash + refill + bonus;
 
-        System.out.println(totalCash);
-        System.out.println(bonus);
+        System.out.println("Итого на счете: " + totalCash);
+        System.out.println("Начислено бонусов:" + bonus);
     }
 }
